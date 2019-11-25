e9420efec4dfb53341e16c3c607022748f16dc4b
diala.ghamdi@gmail.com <you@example.com>
2019-11-25 16:23:40 +0300

    Home page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Navbar, Nav, Form, FormControl, NavbarBrand } from 'react-bootstrap';
 // import {BrowserRouter,Switch,Route}from "react-router-dom";
-import Home from './Component/Home'
+import Home from './Component/home/Home'
 
 
-import Footer from './Component/Footer'
+import Footer from './Component/home/Footer'
 
 
 function App() {
---
+++ front-end/src/Component/home/image.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react'
+import { Row, Col, Container } from 'react-bootstrap';
+import '../../App.css'
+
+
+export default class image extends Component {
+    render() {
+        console.log(this.props.image)
+        return (
+            <Container>
+                <Row>
+                    <Col>
+                        <img src={this.props.image[0]}  className="C1" />
+                    </Col>
+                    <Col>
+                        <Container>
+                            <Row>
+                                <Col>
+                                    <img src={this.props.image[1]}  className="C2"  />
+                                </Col><Col>
+                                    <img src={this.props.image[2]}  className="C3"   />
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                              
+                              
+                                    <img src={this.props.image[3]} className="C4"  />
+                                </Col><Col>
+                                    <img src={this.props.image[4]} className="C5"  />
+                                </Col>
+                            </Row>
+                        </Container>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Container>
+                            <Row>
+                                <Col>
+                                    <img src={this.props.image[5]} className="C6"  />
+                                </Col><Col>
+                                    <img src={this.props.image[6]} className="C7"  />
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <img src={this.props.image[7]} className="C8"  />
+                                </Col><Col>
+                                    <img src={this.props.image[8]} className="C9" />
+                                </Col>
+                            </Row>
+                        </Container>
+                    </Col>
+                    <Col>
+                        <img src={this.props.image[9]} className="C10"  />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <img src={this.props.image[10]} className="C11"  />
+                    </Col>
+                    <Col>
+                        <Container>
+                            <Row>
+                                <Col>
+                                    <img src={this.props.image[11]} className="C12"  />
+                                </Col><Col>
+                                    <img src={this.props.image[12]} className="C13" />
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <img src={this.props.image[13]} className="C14" />
+                                </Col><Col>
+                                    <img src={this.props.image[14]} className="C15"  />
+                                </Col>
+                            </Row>
+                        </Container>
+                    </Col>
+                </Row>
+            </Container>
+        )
+    }
+}
