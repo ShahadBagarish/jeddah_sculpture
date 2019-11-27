af657d46196058db95e15ff11c31aba13e30cbea
diala.ghamdi@gmail.com <you@example.com>
2019-11-27 15:52:24 +0300

    Home page

--- front-end/src/Component/home/Home.js
+++ front-end/src/Component/home/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Button, Navbar, Nav, Form, FormControl, Dropdown, ButtonToolbar, DropdownButton, Jumbotron, SplitButton, Row, Col, Container, Overlay } from 'react-bootstrap';
+import { Button, Navbar, Nav, Form, FormControl, Dropdown, ButtonToolbar, DropdownButton, Jumbotron, SplitButton, Row, Col } from 'react-bootstrap';
 import J1 from "../../img/J1.png"
 import Footer from './Footer'
 import '../../App.css'
@@ -9,134 +9,97 @@ import { Link } from 'react-router-dom';
 import P1 from '../../img/P1.png'
 import { DatePicker, startDate, setStartDate, useState } from 'react-date-picker';
 import NavBar from '../NavBar'
-import userAccount from '../userAccount'
-import P7 from '../../img/P7.png'
+import userccount from '../userAccount'
+
+
 // import { Dropdown } from 'semantic-ui-react'
-
 export default class Home extends Component {
+    state = {
+        data: data,
+        images: [],
+        theme: this.menu,
+    }
+    dropDownmenus = (e => {
+        this.setState({ theme: e.target.value });
+        console.log(this.state.theme);
+    })
+    takeSearchValue = (e => {
+        this.setState({ theme: e.target.value });
+        console.log(this.state.theme);
+    })
     render() {
+        for (let i = 0; i < 15; i++) {
+            this.state.images.push(this.state.data[i].img)
+        }
+        var numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+        numbers = numbers.map(ele => {
+            return <Dropdown.Item href="#/action-2">{ele}</Dropdown.Item>
+        })
+        // console.log(this.state.images.length);
+        // const image = this.state.images.map(img => {
+        //     return <Image image={img}></Image>
+        // })
         return (
-            <div>
-
-                <NavBar />
-
-                <div class="container1">
-                    <div class="navbar navbar-default">
-                        <div class="navbar-inner">
-                            <div class="container">
-                                <ul class="nav navbar-nav">
-                                    {/* <li class="active"><a href="#">Home</a></li> */}
-                                    {/* <li><Link >Profile</Link></li>
-               <li><Link >Booked Activity</Link></li>
-               <li><Link >Password</Link></li> */}
-                                    <img src={P7} className="P7"/>
-
-
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <Container>
-                    <Jumbotron style={{ backgroundColor: "white" }}>
-                        <Form>
-                            <Form.Row>
-                                <Form.Group as={Col} controlId="formGridEmail">
-                                    <Form.Label>Email</Form.Label>
-                                    <Form.Control type="email" placeholder="Enter email" />
-                                </Form.Group>
-
-                               
-                            </Form.Row>
-
-                            <Form.Group controlId="formGridAddress1">
-                                <Form.Label>Address</Form.Label>
-                                <Form.Control placeholder="1234 Main St" />
-                            </Form.Group>
-
-                        
-
-                            <Form.Row>
-                                <Form.Group as={Col} controlId="formGridCity">
-                                    <Form.Label>City</Form.Label>
-                                    <Form.Control />
-                                </Form.Group>
-
-                                <Form.Group as={Col} controlId="formGridState">
-                                    <Form.Label>State</Form.Label>
-                                    <Form.Control as="select">
-                                        <option>Choose...</option>
-                                        <option>...</option>
-                                    </Form.Control>
-                                </Form.Group>
-
-                                <Form.Group as={Col} controlId="formGridZip">
-                                    <Form.Label>Zip</Form.Label>
-                                    <Form.Control />
-                                </Form.Group>
-                            </Form.Row>
-
-
-                            <Button variant="primary" type="submit" >
-                                Save
-  </Button>
-                        </Form>
+            <div className="App">
+                <header>
+                    <NavBar />
+                    <Row className="justify-content-md-center">
+                        <Navbar expand="lg" bg="light" variant="light">
+                            <Nav>
+                                <Nav.Link as={Link} to='/tours' ><p style={{ color: 'black', 'fontSize': '30px' }}>Tours |</p></Nav.Link>
+                                <Nav.Link as={Link} to='/workShops'><p style={{ color: 'black', 'fontSize': '30px' }}>WorkShops |</p></Nav.Link>
+                                <Nav.Link as={Link} to='/scavengerhunts' ><p style={{ color: 'black', 'fontSize': '30px' }}>Scavenger Hunts |</p></Nav.Link>
+                                <Nav.Link as={Link} to='/sharedexperiences'><p style={{ color: 'black', 'fontSize': '30px' }}>Shared Experiences |</p></Nav.Link>
+                                <Nav.Link as={Link} to='/joincommunity' ><p style={{ color: 'black', 'fontSize': '30px' }}>Join Community </p></Nav.Link>
+                            </Nav>
+                        </Navbar>
+                    </Row>
+                </header>
+                <body>
+                    <Jumbotron className="P1">
+                        <h1>Welcome to Jeddah Scuplture Hub</h1>
+                        <Jumbotron style={{ backgroundColor: 'black ', height: '10px', marginTop: '40px' }}>
+                            <Row  >
+                                <Col size={4}>
+                                    <select className="browser-default custom-select"  >
+                                        <option>Number of Participants</option>
+                                        <option value="1"> 1</option>
+                                        <option value="2"> 2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5">5</option>
+                                        <option value="6">6</option>
+                                    </select>
+                                </Col>
+                                <Col siz={4}>
+                                    <select className="browser-default custom-select" value={this.state.theme} onChange={this.dropDownmenus}>
+                                        <option>Choose a theme</option>
+                                        <option value="Islmaic"> Islmaic</option>
+                                        <option value="Geomtric"> Geomtric</option>
+                                        <option value="Human Form">Human Form</option>
+                                        <option value="Natural">Natural</option>
+                                    </select>
+                                </Col>
+                                <Col size={4}>
+                                    <Form.Group controlId='DOJ'>
+                                        <Form.Control
+                                            type="date"
+                                            name="DOJ"
+                                            required
+                                            placeholder="DOJ"
+                                        />
+                                    </Form.Group>
+                                </Col>
+                                <Col size={4}>
+                                    <Button variant="Search" style={{ backgroundColor: 'Yellow' }} onClick={this.takeSearchValue}    >Search</Button>
+                                </Col>
+                            </Row>
+                        </Jumbotron>
                     </Jumbotron>
-                </Container>
-
-                <Container>
-
-                    <Form>
-                        <Form.Group controlId="formBasicPassword">
-                            <Form.Label>Current Password</Form.Label>
-                            <Form.Control type="password" placeholder=" Current Password" />
-                        </Form.Group>
-
-
-
-                        <Form.Group controlId="formBasicPassword">
-                            <Form.Label> New Password</Form.Label>
-                            <Form.Control type="password" placeholder=" New Password" />
-                        </Form.Group>
-
-                        <Form.Group controlId="formBasicPassword">
-                            <Form.Label>  Verify New Password</Form.Label>
-                            <Form.Control type="password" placeholder=" Verify" />
-                        </Form.Group>
-
-                        <Button variant="primary" type="submit">
-                            Submit
-  </Button>
-                    </Form>
-
-                </Container>
-
-
-
-                <Container>
-
-                <Form.Row>
-                                <Form.Group as={Col} >
-                                    <Form.Label>Booked Activities </Form.Label>
-                                    <Form.Control type="input " placeholder=" Your Activity" />
-                                </Form.Group>
-
-                               
-                            </Form.Row>
-
-                            <Form.Row>
-                                <Form.Group as={Col} >
-                                    <Form.Label>Theme of the Activity </Form.Label>
-                                    <Form.Control type="input " placeholder=" Theme of the  Activity" />
-                                </Form.Group>
-
-                               
-                            </Form.Row>
-
-                </Container>
-
-<Footer/>
+                    <h1><center>Most Pouplar Activity</center></h1>
+                    <Image image={this.state.images} />
+                </body>
+                <Footer />
             </div>
         )
     }
--- front-end/src/Component/userAccount.js
+++ front-end/src/Component/userAccount.js
@@ -1,13 +1,139 @@
 import React, { Component } from 'react'
-import { Button, Navbar, Nav, Form, FormControl, Dropdown, ButtonToolbar, DropdownButton, Jumbotron, SplitButton, Row, Col } from 'react-bootstrap';
+import { Button, Navbar, Nav, Form, FormControl, Dropdown, ButtonToolbar, DropdownButton, Jumbotron, SplitButton, Row, Col, Container, Overlay } from 'react-bootstrap';
+import J1 from "../img/J1.png"
+import data from '../data'
+import { Link } from 'react-router-dom';
+import P1 from '../img/P1.png'
+import { DatePicker, startDate, setStartDate, useState } from 'react-date-picker';
 import NavBar from './NavBar'
+import P7 from '../img/P7.png'
+import Footer from './home/Footer'
+// import { Dropdown } from 'semantic-ui-react'
 
 export default class userAccount extends Component {
   render() {
     return (
-    
       <div>
-<NavBar/>
+
+        <NavBar />
+
+        <div class="container1">
+          <div class="navbar navbar-default">
+            <div class="navbar-inner">
+              <div class="container">
+                <ul class="nav navbar-nav">
+                  {/* <li class="active"><a href="#">Home</a></li> */}
+                  {/* <li><Link >Profile</Link></li>
+               <li><Link >Booked Activity</Link></li>
+               <li><Link >Password</Link></li> */}
+                  <img src={P7} className="P7" />
+
+
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <Container>
+          <Jumbotron style={{ backgroundColor: "white" }}>
+            <Form>
+              <Form.Row>
+                <Form.Group as={Col} controlId="formGridEmail">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+
+
+              </Form.Row>
+
+              <Form.Group controlId="formGridAddress1">
+                <Form.Label>Address</Form.Label>
+                <Form.Control placeholder="1234 Main St" />
+              </Form.Group>
+
+
+
+              <Form.Row>
+                <Form.Group as={Col} controlId="formGridCity">
+                  <Form.Label>City</Form.Label>
+                  <Form.Control />
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="formGridState">
+                  <Form.Label>State</Form.Label>
+                  <Form.Control as="select">
+                    <option>Choose...</option>
+                    <option>...</option>
+                  </Form.Control>
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="formGridZip">
+                  <Form.Label>Zip</Form.Label>
+                  <Form.Control />
+                </Form.Group>
+              </Form.Row>
+
+
+              <Button variant="primary" type="submit" >
+                Save
+  </Button>
+            </Form>
+          </Jumbotron>
+        </Container>
+
+        <Container>
+
+          <Form>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Current Password</Form.Label>
+              <Form.Control type="password" placeholder=" Current Password" />
+            </Form.Group>
+
+
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label> New Password</Form.Label>
+              <Form.Control type="password" placeholder=" New Password" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>  Verify New Password</Form.Label>
+              <Form.Control type="password" placeholder=" Verify" />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Submit
+  </Button>
+          </Form>
+
+        </Container>
+
+
+
+        <Container>
+
+          <Form.Row>
+            <Form.Group as={Col} >
+              <Form.Label>Booked Activities </Form.Label>
+              <Form.Control type="input " placeholder=" Your Activity" />
+            </Form.Group>
+
+
+          </Form.Row>
+
+          <Form.Row>
+            <Form.Group as={Col} >
+              <Form.Label>Theme of the Activity </Form.Label>
+              <Form.Control type="input " placeholder=" Theme of the  Activity" />
+            </Form.Group>
+
+
+          </Form.Row>
+
+        </Container>
+
+        <Footer />
       </div>
     )
   }
