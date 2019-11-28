dfe36c34509068a3e73df5a0f06348663d56b2ec
Tareq Alotaibi <tareqdev@protonmail.com>
2019-11-28 10:09:00 +0300

    Read me / admin

--- routes/DashBoard.jsx
+++ routes/DashBoard.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { WrapperBox , DashboardHeader , ValueBlock , Columns , Column} from 'admin-bro/components'
+import { WrapperBox , DashboardHeader } from 'admin-bro/components'
 import {  } from 'admin-bro/components'
 
 export default class DashBoard extends Component {
@@ -22,21 +22,9 @@ export default class DashBoard extends Component {
 
 
       <DashboardHeader>
-         <h1>Overlaying text</h1>
+         <h1>أنا تعبانة خلاص</h1>
        </DashboardHeader>
-       <WrapperBox>
-         <Columns style={{marginTop: '-80px'}}>
-           <Column><ValueBlock  icon="fa fa-bomb" value="5">
-             Utils
-           </ValueBlock></Column>
-           <Column><ValueBlock  icon="fa fa-star" value="12">
-             Are
-           </ValueBlock></Column>
-           <Column><ValueBlock  icon="fa fa-cog" value="5" color="red">
-             Awesome
-           </ValueBlock></Column>
-         </Columns>
-       </WrapperBox>
+
 
 
 
